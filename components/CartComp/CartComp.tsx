8e1290de2503c555e2fc36532f045799bedaf61c
Mohammad Ahsan Habib <ahsan.h.86@gmail.com>
2022-11-24 16:16:15 +0600

    Update

--- components/CartComp/CartComp.tsx
+++ components/CartComp/CartComp.tsx
@@ -1,73 +1,74 @@
-import { Button, Card, Col, Empty, Row } from 'antd';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectCart } from '../../store/reducers/cartReducer';
-import { ProductDataTypes } from '../ProductList/ProductList.types';
-import Styles from './CartComp.module.scss';
-import CartItem from './CartItems';
+import { Button, Card, Col, Empty, Row } from 'antd'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectCart } from '../../store/reducers/cartReducer'
+import { ProductDataTypes } from '../ProductList/ProductList.types'
+import Styles from './CartComp.module.scss'
+import CartItem from './CartItems'
 
 const CartComp = () => {
-    const router = useRouter();
-    // @ts-ignore
-    const { cartItems, totalItems } = useSelector(selectCart);
-    const dispatch = useDispatch()
-    const handleCheckout = () => {
-        router.push('/sign-in?redirect=shipping')
-    }
-    return (
-        <div className={Styles.cart_page}>
-            <h3>Cart Items</h3>
+  const router = useRouter()
+  // @ts-ignore
+  const { cartItems, totalItems } = useSelector(selectCart)
+  const dispatch = useDispatch()
+  const handleCheckout = () => {
+    router.push('/sign-in?redirect=shipping')
+  }
+  return (
+    <div className={Styles.cart_page}>
+      <h3>Cart Items</h3>
 
-            {cartItems?.length ?
-                <Row gutter={30}>
-                    <Col span={17} className={Styles.cart_items}>
-                        {cartItems?.map((item: ProductDataTypes) => (
-                            <Card key={item._id}>
-                                <CartItem propObj={{ dispatch, item }} />
-                            </Card>
-                        ))}
-                    </Col>
+      {cartItems?.length ? (
+        <Row gutter={30}>
+          <Col span={17} className={Styles.cart_items}>
+            {cartItems?.map((item: ProductDataTypes) => (
+              <Card key={item._id}>
+                <CartItem propObj={{ dispatch, item }} />
+              </Card>
+            ))}
+          </Col>
 
-                    <Col span={7} className={Styles.process_to_checkout}>
-                        <Card>
-                            <div>
-                                <h4>
-                                    Subtotal: {" "}
-                                    <span className={Styles.subtotal}>
-                                        ${cartItems.reduce((a: number, c: ProductDataTypes) => a + ((c?.price * (c?.quantity || 0))), 0)}
-                                    </span>
-                                    {" "}
-                                    <span className={Styles.total_quantity}>
-                                        {totalItems} items
-                                    </span>
-                                </h4>
-                                <Button onClick={handleCheckout} type='primary' style={{ backgroundColor: "#faad14", borderColor: "#faad14", color: "#333" }}>
-                                    Proceed to Checkout
-                                </Button>
-                                <br />
-                                or
-                                <br />
-                                <Link href="/">Browse more products</Link>
-                            </div>
-                        </Card>
-                    </Col>
-                </Row> :
-                <Empty
-                    description={
-                        <span>
-                            Cart is empty!{' '}
-                            <Link href="/">Browse products</Link>
-                        </span>
-                    }>
-                </Empty>
-            }
-
-        </div>
-    )
+          <Col span={7} className={Styles.process_to_checkout}>
+            <Card>
+              <div>
+                <h4>
+                  Subtotal:{' '}
+                  <span className={Styles.subtotal}>
+                    $
+                    {cartItems.reduce(
+                      (a: number, c: ProductDataTypes) => a + c?.price * (c?.quantity || 0),
+                      0
+                    )}
+                  </span>{' '}
+                  <span className={Styles.total_quantity}>{totalItems} items</span>
+                </h4>
+                <Button
+                  onClick={handleCheckout}
+                  type="primary"
+                  style={{ backgroundColor: '#faad14', borderColor: '#faad14', color: '#333' }}
+                >
+                  Proceed to Checkout
+                </Button>
+                <br />
+                or
+                <br />
+                <Link href="/">Browse more products</Link>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      ) : (
+        <Empty
+          description={
+            <span>
+              Cart is empty! <Link href="/">Browse products</Link>
+            </span>
+          }
+        ></Empty>
+      )}
+    </div>
+  )
 }
 
 export default CartComp
-
-
-
