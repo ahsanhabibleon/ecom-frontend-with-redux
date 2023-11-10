835fc8b0827f5091f2eec73dfba4934b627c7cb4
Ahsan Habib <ahsan.habib@shikho.com>
2023-11-11 01:56:55 +0600

    F

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,33 +1,53 @@
 import React from 'react'
 import { ArrowRightOutlined } from '@ant-design/icons'
 import { Button, Carousel } from 'antd'
-import Styles from "./Banner.module.scss"
+import Styles from './Banner.module.scss'
 import { bannerData } from './data'
+import axios from 'axios'
 
 const Banner = () => {
-    return (
-        <div className={Styles.banner_wrapper}>
-            <div className="container">
-                    <button role="button">Click me</button>
-                <Carousel autoplay>
-                    {bannerData.map((ban => (
-                        <div key={ban.key}>
-                            <div className={Styles.banner} style={{ backgroundImage: `url(${ban?.backgroundImg})` }}>
-                                <div className={Styles.banner_content}>
-                                    <h1>{ban?.title || 'Banner Title'}</h1>
-                                    <Button  href='#product-list'>
-                                        Explore
-                                        <ArrowRightOutlined />
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-                    )))}
-                </Carousel>
+  const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
+  const handleClick = async () => {
+    axios
+      .get(`/api/users`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((data) => {
+        console.log(data?.data)
+      })
+      .catch((error) => {
+        console.log({ error })
+      })
+  }
+  return (
+    <div className={Styles.banner_wrapper}>
+      <div className="container">
+        <button role="button" onClick={handleClick}>
+          Click me
+        </button>
+        <Carousel autoplay>
+          {bannerData.map((ban) => (
+            <div key={ban.key}>
+              <div
+                className={Styles.banner}
+                style={{ backgroundImage: `url(${ban?.backgroundImg})` }}
+              >
+                <div className={Styles.banner_content}>
+                  <h1>{ban?.title || 'Banner Title'}</h1>
+                  <Button href="#product-list">
+                    Explore
+                    <ArrowRightOutlined />
+                  </Button>
+                </div>
+              </div>
             </div>
-        </div>
-
-    )
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  )
 }
 
 export default Banner
